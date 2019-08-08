d8eb42620f15d8789a8e7e9230ff34f59205e76b
Parker <pcmayer0@gmail.com>
2019-08-08 16:39:45 -0700

    initial commit

---
+++ HelloWorldDR4BL.cpp
@@ -0,0 +1,45 @@
+/*#include <Arduino.h>
+
+int leftPinINA = 12;
+int leftPinINB = 11;
+int rightPinINA = 9;
+int rightPinINB = 8;
+
+int BUTTON = 7;
+
+bool robotReady = false;
+
+void setup() {
+
+	pinMode(leftPinINA, OUTPUT);
+	pinMode(leftPinINB, OUTPUT);
+	pinMode(rightPinINA, OUTPUT);
+	pinMode(rightPinINB, OUTPUT);
+
+	pinMode(BUTTON, INPUT_PULLUP);
+
+	//pinMode(13, OUTPUT);
+}
+
+void loop() {
+
+	if(digitalRead(BUTTON) == LOW){
+		robotReady = true;
+	}
+
+	if(robotReady){
+		analogWrite(leftPinINA, 127);
+		analogWrite(rightPinINB, 127);
+		delay(1000);
+		analogWrite(leftPinINA, 0);
+		analogWrite(rightPinINB, 0);
+
+		robotReady = false;
+		/*digitalWrite(13, 0);
+		delay(1000);
+		digitalWrite(13, 1);
+		delay(1000);
+	}
+
+}*/
+
